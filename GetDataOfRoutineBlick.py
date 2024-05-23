59a26f018efbe0c8c89f245527c8321aa594b096
MTiefengraber <123746871+MTiefengraber@users.noreply.github.com>
2024-05-23 16:17:42 +0200

    removed unnecessary imports

--- GetDataOfRoutineBlick.py
+++ GetDataOfRoutineBlick.py
@@ -6,9 +6,7 @@ import os
 path = os.path.normpath("C:/Blick/src")
 os.chdir(path)
 sys.path.insert(0, path)
-from numpy import arange, polyval, ones, dstack, \
-    isnan, where, nan, array, split, argmin, argmax
-from glob import glob
+from numpy import dstack
 from blick_psio import *
 psio = blick_psio()
 
