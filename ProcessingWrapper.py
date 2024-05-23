27f64129d0fedf3cc5ed940518282a7f8e5ba546
MTiefengraber <123746871+MTiefengraber@users.noreply.github.com>
2024-05-23 11:39:42 +0200

    Update ProcessingWrapper.py  -  prior commit wasn't working

--- ProcessingWrapper.py
+++ ProcessingWrapper.py
@@ -4,7 +4,8 @@
 
 # IMPORT modules
 import os
-from blickp_processingsetups import FittingSetup
+import h5py
+
 # Define the desired path
 path = os.path.normpath("C:/Blick/src")
 os.chdir(path)
@@ -21,7 +22,7 @@ from blickp import create_processor_adv
 
 def ProcessingWrapper(instrnam, instrnum, specnum, locname, fcode,
                     pthOF, pthCF, sBlickRootPth, pthL0, pthL1, pthL2, pthL2Fit, pthPF, currday,
-                    par, sRefRtn, iDateC, sDateRefC, sSCode, sCfSuffix):
+                    par, sRefRtn, iDateC, sDateRefC, sSCode, sRefType, sCfSuffix):
 
     #Initialize empty dictionary
     dirs = {}
@@ -30,61 +31,37 @@ def ProcessingWrapper(instrnam, instrnum, specnum, locname, fcode,
     dirs['blick_dir'] = sBlickRootPth
     dirs['data_dir_L0'] = pthL0
     dirs['data_dir_L1'] = pthL1
-    dirs['data_dir_L2Fit'] = pthL2Fit
+    L2FitPth = os.path.join(pthL2Fit, sRefType)
+    if not os.path.exists(L2FitPth):
+        os.makedirs(L2FitPth)
+    dirs['data_dir_L2Fit'] = L2FitPth
     dirs['data_dir_L2'] = pthL2
     dirs['data_dir_iof'] = sBlickRootPth + pthOF
     dirs['data_dir_icf'] = sBlickRootPth + pthCF
 
+    from main_makeCompData import GetExternalReferenceFileName
+    with h5py.File(pthPF, 'r+') as pssetup:
+        idxFCode = where(pssetup['f_codes'][:,0]['f-code'] == fcode)[0][0]
+        FCode = pssetup['f_codes'][idxFCode]
+        if sRefType.startswith('Ref'):
+            sRefNme = GetExternalReferenceFileName(par, instrnum, specnum, sRefRtn, FCode['Filter types'][0], iDateC, sDateRefC,
+                                                   sSCode[0])
+            sFCodeRefNme = string_('Ref_' + sRefNme)
+        else:
+            sFCodeRefNme = sRefType
+        FCode['Reference'] = array([sFCodeRefNme], dtype='|S100')
+        pssetup['f_codes'][idxFCode] = FCode
+
     # For the O3VIS product, the processor has to run for both calibration files (Finkenzeller, 2022; ThalmanAndVolkamer, 2013)
     cal_file_name = instrnam + str(instrnum) + 's' + str(specnum) + '_CF_' + sCfSuffix + '.txt'
+
     processor = create_processor_adv(dirs, instrnum, specnum, locname, None, fcode, None, cal_file=cal_file_name,
                                      proc_setup=pthPF, nuke_l2=True)
-
-    reference = processor.config.processing_setups.fcodes[fcode].reference
-    # Check if reference starts with REF_ and overwrite it with reference data
-    if reference.startswith('REF_'):
-        # Overwrite product reference type in f-code
-        copy = processor.config.processing_setups.fcodes[fcode]
-
-        from main_makeCompData import GetExternalReferenceFileName
-        sRefNme = GetExternalReferenceFileName(par, instrnum, specnum, sRefRtn, copy.filter_types, iDateC, sDateRefC, sSCode[0])
-
-        processor.config.processing_setups.fcodes[fcode] = FittingSetup(
-            code=copy.code,
-            name=copy.name,
-            processing_types=copy.processing_types,
-            filter_types=copy.filter_types,
-            reference=string_('REF_' + sRefNme), # Overwrite product reference type in f-code
-            wlstart=copy.wlstart,
-            wlend=copy.wlend,
-            npoll=copy.npoll,
-            noffs=copy.noffs,
-            nwlc=copy.nwlc,
-            nresc=copy.nresc,
-            fitted_gases=copy.fitted_gases,
-            gas_sources=copy.gas_sources,
-            gas_od_methods=copy.gas_od_methods,
-            gas_temperatures=copy.gas_temperatures,
-            ring=copy.ring,
-            molecular_scattering=copy.molecular_scattering,
-            linear_fitting=copy.linear_fitting,
-            uncertainty=copy.uncertainty,
-            scodes=copy.scodes,
-            diffuse_correction=copy.diffuse_correction,
-            time_interploation=copy.time_interploation,
-            pixels_to_use=copy.pixels_to_use,
-            qf_code=copy.qf_code,
-            date=copy.date,
-            author=copy.author)
-
-    else:
-        print('Reference not REF_')
 
     dd = str(currday)
     dd = date(int(dd[:4]), int(dd[4:6]), int(dd[6:]))
     processor.process_day(dd)
 
-    curr_ref = str(reference)
     print("Poor man's wrapper finished")
 
     return curr_ref
